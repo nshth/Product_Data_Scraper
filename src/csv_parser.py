ff95a0759c1ad70079af592ad368ba7f9867a619
Nashath Rasha <nashathrasha0@gmail.com>
2025-10-28 14:25:58 +0530

    Implement base csv parsing and scraping func

--- src/csv_parser.py
+++ src/csv_parser.py
@@ -2,3 +2,39 @@
 # iterate each and get product link
 # and get variant names and quantity and total quantity
 # pass product link, variant names to scraper.py
+
+import pandas as pd
+
+product_list = []
+
+def parse_csv():
+    products = pd.read_csv('data/input/demo.csv', quotechar='"')
+    product_list = []
+    
+    for _, product in products.iterrows():
+        product_link = product['Product_link']
+
+        v1 = product["Variant_1"]
+        v1_quantity = product["V1_quantity"]
+
+        v2 = product["Variant_2"]
+        v2_quantity = product["V2_quantity"]
+
+        v3 = product["Variant_3"]
+        v3_quantity = product["V3_quantity"]
+
+        total_quantity = product["total_quantity"]
+
+        product_data = {
+            'product_link': product_link,
+            'variants': [
+                {'name': v1, 'quantity': v1_quantity},
+                {'name': v2, 'quantity': v2_quantity},
+                {'name': v3, 'quantity': v3_quantity}
+            ],
+            'total_quantity': total_quantity
+        }
+        
+        product_list.append(product_data)
+    
+    return product_list 
--- src/scraper.py
+++ src/scraper.py
@@ -1,3 +1,40 @@
 # open a browser session using Playwright
 # wait
 # scrape with beautifulsoup
+
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+from csv_parser import parse_csv
+
+def get_page_content(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        # page.wait_for_selector() 
+        content = page.content()
+        browser.close()
+        return content
+    
+def parse_with_beautifulsoup(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    div = soup.find(class_='title--wrap--UUHae_g')
+    title = div.get_text()
+    
+    return title
+
+if __name__ == "__main__":
+    products = parse_csv()  
+    
+    for product in products:
+        target_url = product['product_link']
+        variants = product['variants']
+        total_qty = product['total_quantity']
+                
+        html_content = get_page_content(target_url)
+        if html_content:
+            print(f"Successfully fetched HTML content")
+            soup_object = parse_with_beautifulsoup(html_content)
+            print(soup_object)
+        else:
+            print(f"Failed to fetch HTML content")
